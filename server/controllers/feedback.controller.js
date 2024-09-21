12032c99c47811c98334874be5c502dedd27a20c
siddesh <siddhesh112004@gmail.com>
2024-09-21 23:09:18 +0530

    modifying the feedback.controller.js

--- server/controllers/feedback.controller.js
+++ server/controllers/feedback.controller.js
@@ -5,14 +5,17 @@ import ApiRespoance from "../utils/ApiResponse.js";
 
 const feedbackcollection=Asynchandler(async(req,res)=>{
     const {data}=req.body;
-
+     if(!data){
+        throw new ApiError(400,"please provide data")
+     }
       data.map(async(element)=>{
        const feedback = await Feedback.create({
             clientid:req.user._id,
             email:element.email,
             feedback:element.feedback
         })
-     
+        console.log(feedback)
+        
       })
      res.status(200).json(
         new ApiRespoance(200,"feedback submitted")
