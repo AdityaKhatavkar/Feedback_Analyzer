40a536162acb703846f6053a816198dd33538da7
siddesh <siddhesh112004@gmail.com>
2024-09-26 23:03:58 +0530

    email verifcation page is created

--- client/src/components/emailverfication/Emailverification.jsx
+++ client/src/components/emailverfication/Emailverification.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 function Emailverification() {
-  return (
-    <div className='h-screen flex flex-col items-center justify-center gap-4'>
-        <div>
-            ss
+    return (
+        <div className='h-screen flex flex-col items-center justify-center gap-4'>
+            <div className='font-bold text-2xl'>Email verification</div>
+            <div className='font-bold text-2xl flex flex-row gap-4'>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <button className="btn btn-primary">Primary</button>
+            </div>
+            <p>check your email for verification code</p>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Emailverification
