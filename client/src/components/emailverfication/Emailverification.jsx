10cdee993256a38bea9a1763e03218885b7cd92f
siddesh <siddhesh112004@gmail.com>
2024-09-26 23:06:56 +0530

    changes

--- client/src/components/emailverfication/Emailverification.jsx
+++ client/src/components/emailverfication/Emailverification.jsx
@@ -5,8 +5,8 @@ function Emailverification() {
         <div className='h-screen flex flex-col items-center justify-center gap-4'>
             <div className='font-bold text-2xl'>Email verification</div>
             <div className='font-bold text-2xl flex flex-row gap-4'>
-                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                <button className="btn btn-primary">Primary</button>
+                <input type="text" placeholder="Verification code" className="input input-bordered w-full max-w-xs" />
+                <button className="btn btn-primary">verify</button>
             </div>
             <p>check your email for verification code</p>
         </div>
