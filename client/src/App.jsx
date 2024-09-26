ed4c0c97f9809ad908e3f0dab19ba83e3ee0ca28
siddesh <siddhesh112004@gmail.com>
2024-09-26 22:56:54 +0530

    react steup and login page and singup page step up

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from './components/login/Login.jsx'
+import Signup from './components/Signup/Signup.jsx'
+import Emailverification from './components/emailverfication/Emailverification.jsx'
+function App() {
+  return (
+    <>
+    
+        <Signup/>
+    </>
+
+  )
+}
+
+export default App
---
+++ client/src/components/emailverfication/Emailverification.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Emailverification() {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center gap-4'>
+        <div>
+            ss
+        </div>
+    </div>
+  )
+}
+
+export default Emailverification
