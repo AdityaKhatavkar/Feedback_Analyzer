82862132d4c9d91fc495cdfed3e4ba1ab2cdc6c0
siddesh <siddhesh112004@gmail.com>
2024-09-29 18:08:31 +0530

    modifying the routes

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
+import { Link } from 'react-router-dom'
 function Signup() {
     const [fullname, setFullname] = useState('')
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-      
 
-    const handlesignup=async(e)=>{
+
+    const handlesignup = async (e) => {
         e.preventDefault();
-        if(fullname===''||username===''||email===''||password===''){
+        if (fullname === '' || username === '' || email === '' || password === '') {
             toast.error("Please fill all the fields")
-            return ;
+            return;
         }
-        try{
-              
+        try {
+
         }
-        catch{
-            
+        catch {
+
         }
-        
+
 
     }
 
@@ -36,8 +37,7 @@ function Signup() {
                             "Analyze user feedback through our website to gain insights and improve services. Sign up now!"
                         </p>
                         <label className="label">
-                            <a href="#" className="label-text-alt link link-hover text-blue-600 text-sm">Already have an account?</a>
-                        </label>
+                            <Link to="/login" className="label-text-alt link link-hover text-blue-600 text-sm">Already have account?</Link>                        </label>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <form className="card-body">
