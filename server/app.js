f1c42c923ae83a6733f0634b900e33ac06c6c9b2
siddesh <siddhesh112004@gmail.com>
2024-10-02 12:07:39 +0530

    adding the global catch

--- server/app.js
+++ server/app.js
@@ -10,4 +10,15 @@ import router from "./routes/user.route.js";
 
 app.use('/user',router);
 
+app.use((err,req,res,next)=>{
+    const errorStatus=err.status || 500;
+    const errorMessage=err.message || "Something went wrong";
+    return res.status(errorStatus).json({
+        success:false,
+        status:errorStatus,
+        message:errorMessage,
+        stack:err.stack
+    })
+})
+
 export {app};
